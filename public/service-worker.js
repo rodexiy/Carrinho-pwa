7e4effb9ac89726fe884143fd265a4906298ed94
Daniel Luis da Rocha <daniellr@weg.net>
2024-02-23 18:14:35 -0300

    daijdaw

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,6 @@
 let cachePwa = "cachePwa-v1"
 
 let assets = [
-  "/",
-  "/192x192.png",
-  "/512x512.png",
   "/lixeira.png",
   "/manifest.json",
   "/service-worker.js",
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
